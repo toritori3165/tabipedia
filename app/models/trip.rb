187178f3f06ddef21e401bf74cecb81eb20204e1
toritori3165 <155225220+toritori3165@users.noreply.github.com>
2024-02-23 18:39:57 +0900

    色々しました

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,8 +1,8 @@
 class Trip < ApplicationRecord
-  has_many :users, through: :trip_users
   has_many :trip_users, dependent: :destroy
+  has_many :users, through: :trip_users
   has_many :plans, dependent: :destroy
-  accepts_nested_attributes_for :trip_users
+  
 
 
   with_options presence: true do
