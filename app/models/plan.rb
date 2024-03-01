6df2fa85c65ba61457a69c038b2b5fe6e644ede0
ranoooooooooooo <rubbed1it9with6the9bow@gmail.com>
2024-03-01 10:14:32 +0900

    いじった

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,9 +1,15 @@
 class Plan < ApplicationRecord
   belongs_to :trip
+  has_many_attached :images
 
   with_options presence: true do
+    validates :days_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :plan_title
     validates :content
     validates :trip_id
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :days
+
 end
--- db/migrate/20240222090318_create_plans.rb
+++ db/migrate/20240222090318_create_plans.rb
@@ -1,6 +1,7 @@
 class CreatePlans < ActiveRecord::Migration[7.0]
   def change
     create_table :plans do |t|
+      t.integer :days_id,    null: false
       t.string :plan_title,  null: false
       t.text :content,       null: false
       t.references :trip,    null: false, foreign_key: true
